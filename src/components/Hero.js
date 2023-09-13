74c6b3d5c42437700fbb3730633ab6cf891892d8
Tchowdhury1 <Tashchowdhury1@gmail.com>
2023-09-13 19:24:41 +1000

    changes to add email verification error

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import logo from "../assets/logo.svg";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-const Hero = () => (
-  <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+const Hero = () => {
 
-    <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
-    </p>
-  </div>
-);
+    const {
+        user,
+        isAuthenticated,
+    } = useAuth0();
+
+    let warning;
+    if (isAuthenticated && !user.email_verified) {
+        warning = <div className="email-warning"><span>You need to verify your email address!</span></div>
+    }
+
+    return (
+        <div className="text-center hero my-5">
+            {warning}
+            <img className="mb-3 app-logo" src={logo} alt="React logo" width="120"/>
+            <h1 className="mb-4">React.js Sample Project</h1>
+
+            <p className="lead">
+                This is a sample application that demonstrates an authentication flow for
+                an SPA, using <a href="https://reactjs.org">React.js</a>
+            </p>
+        </div>
+    );
+};
 
 export default Hero;
